53d83450cccc3fc6e892e24a18afd5aa2f91bcd1
Blendi Zeqiri <blendizeqiri@hotmail.com>
2024-02-04 04:30:20 -0800

    Created app

---
+++ IOS_Project/Shared/IOS_ProjectApp.swift
@@ -0,0 +1,21 @@
+//
+//  IOS_ProjectApp.swift
+//  Shared
+//
+//  Created by BZ on 2/4/24.
+//
+
+import SwiftUI
+
+@available(iOS 11.0, macOS 10.15, *)
+struct Testing_SwiftUI2AppHolder {
+    @main
+    struct Testing_SwiftUI2App: App {
+
+        var body: some Scene {
+            WindowGroup {
+                ContentView()
+            }
+        }
+    }
+}
